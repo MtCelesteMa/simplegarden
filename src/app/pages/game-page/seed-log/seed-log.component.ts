38791cf51ebccc17572d1732d83aed43ccdf22d4
Celeste Ma <mtcelestema@proton.me>
2024-07-28 06:29:41 +0800

    Added seed log search.

--- src/app/pages/game-page/seed-log/seed-log.component.ts
+++ src/app/pages/game-page/seed-log/seed-log.component.ts
@@ -1,6 +1,8 @@
 import { Component, inject } from "@angular/core";
+import { ReactiveFormsModule, FormControl } from "@angular/forms";
 import { MatCardModule } from "@angular/material/card";
-import { MatGridList } from "@angular/material/grid-list";
+import { MatInputModule } from "@angular/material/input";
+import { MatIconModule } from "@angular/material/icon";
 import { ManagerService } from "../../../services/manager.service";
 import { SeedLogEntryComponent } from "./seed-log-entry/seed-log-entry.component";
 import * as g from "../../../../game";
@@ -8,12 +10,14 @@ import * as g from "../../../../game";
 @Component({
     selector: "app-seed-log",
     standalone: true,
-    imports: [SeedLogEntryComponent, MatCardModule, MatGridList],
+    imports: [ReactiveFormsModule, SeedLogEntryComponent, MatCardModule, MatInputModule, MatIconModule],
     templateUrl: "./seed-log.component.html",
     styleUrl: "./seed-log.component.scss",
 })
 export class SeedLogComponent {
     manager = inject(ManagerService);
+
+    cropSearch = new FormControl<string>("");
 
     get nCrops(): number {
         return Object.entries(this.manager.game!.gameData.crops).length;
@@ -23,11 +27,10 @@ export class SeedLogComponent {
         return Object.keys(this.manager.game!.saveData.cropsUnlocked).length;
     }
 
-    get listUnlockedCrops(): string[] {
+    get listUnlockedCrops(): [string, g.d.g.v2.CropInfo][] {
         return Object.entries(this.manager.game!.gameData.crops)
             .filter((value: [string, g.d.g.v2.CropInfo]): boolean => {
                 return Object.hasOwn(this.manager.game!.saveData.cropsUnlocked, value[0]);
             })
-            .map((value: [string, g.d.g.v2.CropInfo]): string => value[0]);
     }
 }
